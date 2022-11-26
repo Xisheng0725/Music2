5161692a9924c9ef25d6423b361bf0daa151c919
zhangxisheng <xzhang3033@gatech.edu>
2022-11-26 16:17:26 -0500

    react

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '1aee870fccdb4d3cae9de281ffed5f7b',
+        ClientSecret: '8feee0b1b9544a0784fdbf309b522697'
+    }
+}
+
+export { Credentials };
