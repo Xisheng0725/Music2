0f5c87c3c48ddd35f53a2628195a87d372a6e0d6
Kent Barber <kbarber9@gatech.edu>
2022-11-29 23:04:28 -0500

    finished tests

--- backend/src/test/java/group8/demo/ProAuxApplicationTests.java
+++ backend/src/test/java/group8/demo/ProAuxApplicationTests.java
@@ -1,5 +1,7 @@
 package group8.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.concurrent.ExecutionException;
 
 import org.junit.jupiter.api.Test;
@@ -15,31 +17,31 @@ class ProAuxApplicationTests {
 	void contextLoads() {
 	}
 
-	// public void createUserData() throws ExecutionException, InterruptedException {
-	// 	User user = new User();
-	// 	user.setEmail("kbarber0199@gmail.com");
-	// 	user.setPassword("Password");
-	// 	userService.createUser(user);
-	// }
+	public void createUserData() throws ExecutionException, InterruptedException {
+		User user = new User();
+		user.setEmail("kbarber0199@gmail.com");
+		user.setPassword("Password");
+		userservice.createUser(user);
+	}
 
-	// @Test
-	// public void testUserData() throws ExecutionException, InterruptedException {
-	// 	createUserData();
-	// 	boolean res = userservice.existsUser("kentster");
-	// 	assertEquals(true, res);
-	// }
+	@Test
+	public void testUserData() throws ExecutionException, InterruptedException {
+		createUserData();
+		boolean res = userservice.findEmail("kbarber0199@gmail.com");
+		assertEquals(true, res);
+	}
 
-	// @Test
-	// public void testNotUser() throws ExecutionException, InterruptedException {
-	// 	boolean res = userservice.existsUser("notHere");
-	// 	assertEquals(false, res);
-	// }
+	@Test
+	public void testNotUser() throws ExecutionException, InterruptedException {
+		boolean res = userservice.findEmail("notHere");
+		assertEquals(false, res);
+	}
 
-	// @Test
-	// public void matchPasswords() throws ExecutionException, InterruptedException {
-	// 	createUserData();
-	// 	String res = userservice.getPasswordByUsername("kentster");
-	// 	assertEquals("Password", res);
-	// }
+	@Test
+	public void matchPasswords() throws ExecutionException, InterruptedException {
+		createUserData();
+		String res = userservice.getPasswordByEmail("kbarber0199@gmail.com");
+		assertEquals("Password", res);
+	}
 
 }
