78110cb806ec4a67a3db0d83410b2bf1231d9e90
addyyang <62062723+addyyang@users.noreply.github.com>
2022-12-01 14:00:04 -0500

    Update app.service.ts

--- frontend-login/src/app/app.service.ts
+++ frontend-login/src/app/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,22 @@ export class AppService {
   URL = 'http://localhost:8080'
 
   sendEmail(email) {
-    return this.http.post(this.URL + '/users/setEmail/'+email, {"email": email});
+    return this.http.post(this.URL + '/users/set-email/'+email, {"email": email});
   }
 
   getEmail() {
-    return this.http.get(this.URL + '/users/getEmail');
+    return this.http.get(this.URL + '/users/get-email').pipe(map(r=>r));
   }
 
   findEmail(email) {
-    return this.http.post(this.URL + '/users/find-email/', {"email": email});
+    return this.http.post(this.URL + '/users/find-email/', {"email": email}).pipe(map(r=>r));
   }
 
   matchPass(email, pass) {
-    return this.http.post(this.URL + '/users/match-credentials/', {"email": email, "password": pass});
+    return this.http.post(this.URL + '/users/match-credentials/', {"email": email, "password": pass}).pipe(map(r=>r));
   }
 
   createUser(email, pass) {
-    return this.http.post(this.URL + 'users/new-user', {"email": email, "password": pass});
+    return this.http.post(this.URL + '/users/new-user', {"email": email, "password": pass});
   }
 }
