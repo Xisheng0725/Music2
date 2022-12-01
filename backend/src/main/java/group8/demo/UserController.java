2cf691999a2d0149f81c21afb9a20e723ac323e1
Addy Yang <yang.addy@hotmail.com>
2022-12-01 14:24:19 -0500

    CORS config

--- backend/src/main/java/group8/demo/UserController.java
+++ backend/src/main/java/group8/demo/UserController.java
@@ -3,7 +3,6 @@ import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
 
 @RestController
 @CrossOrigin
@@ -13,28 +12,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/setEmail")
+    @PostMapping("/set-email")
     public void setEmail(@RequestBody Email email) throws InterruptedException, ExecutionException {
         userService.setEmail(email.getEmail());
     }
 
-    @GetMapping("/getEmail")
+    @GetMapping("/get-email")
     public String getEmail() throws InterruptedException, ExecutionException {
         return userService.getEmail();
     }
 
-    @PostMapping("/findEmail")
+    @PostMapping("/find-email")
     public boolean findEmail(@RequestBody Email email) throws InterruptedException, ExecutionException {
         return userService.findEmail(email.getEmail());
     }
 
 	@PostMapping("/match-credentials")
-	public boolean getPassword (@RequestBody User user) throws InterruptedException, ExecutionException {
+	public boolean matchCredentials(@RequestBody User user) throws InterruptedException, ExecutionException {
         return (userService.getPasswordByEmail(user.getEmail()).equals(user.getPassword()));
 	}
 
     @PostMapping("/new-user")
-    @ResponseStatus(HttpStatus.CREATED)
     public void createUser(@RequestBody User user) throws ExecutionException, InterruptedException{
         userService.createUser(user);
     }
